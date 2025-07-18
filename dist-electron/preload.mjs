8a4dc706383c981591ac265c07d4f20919f7f18b
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-07-18 13:52:28 +0800

    build: update build config and assets for electron app
    - Add new icon assets for different platforms (ico, icns, png)
    - Change main entry point to dist-electron/main.js
    - Format preload.mjs for better readability

--- dist-electron/preload.mjs
+++ dist-electron/preload.mjs
@@ -1,1 +1,31 @@
-"use strict";const o=require("electron"),{contextBridge:r,ipcRenderer:e}=o;r.exposeInMainWorld("electronAPI",{getSystemInfo:()=>e.send("get-system-info"),onSystemInfoResponse:t=>{const n=(s,i)=>t(i);return e.on("systemInfoResponse",n),()=>{e.removeListener("systemInfoResponse",n)}},onUpdateMetrics:t=>{const n=(s,i)=>t(i);return e.on("updateMetrics",n),()=>{e.removeListener("updateMetrics",n)}},analyzeJunkFiles:()=>e.invoke("analyze-junk-files"),executeCleaning:t=>e.invoke("execute-cleaning",t),getDiskUsage:()=>e.invoke("get-disk-usage"),getNetworkActivity:()=>e.invoke("get-network-activity"),showReminderNotification:(t,n,s)=>e.send("show-reminder-notification",t,n,s),setSystemMetricsInterval:t=>e.send("set-system-metrics-interval",t)});
+"use strict";
+const o = require("electron"),
+  { contextBridge: r, ipcRenderer: e } = o;
+r.exposeInMainWorld("electronAPI", {
+  getSystemInfo: () => e.send("get-system-info"),
+  onSystemInfoResponse: (t) => {
+    const n = (s, i) => t(i);
+    return (
+      e.on("systemInfoResponse", n),
+      () => {
+        e.removeListener("systemInfoResponse", n);
+      }
+    );
+  },
+  onUpdateMetrics: (t) => {
+    const n = (s, i) => t(i);
+    return (
+      e.on("updateMetrics", n),
+      () => {
+        e.removeListener("updateMetrics", n);
+      }
+    );
+  },
+  analyzeJunkFiles: () => e.invoke("analyze-junk-files"),
+  executeCleaning: (t) => e.invoke("execute-cleaning", t),
+  getDiskUsage: () => e.invoke("get-disk-usage"),
+  getNetworkActivity: () => e.invoke("get-network-activity"),
+  showReminderNotification: (t, n, s) =>
+    e.send("show-reminder-notification", t, n, s),
+  setSystemMetricsInterval: (t) => e.send("set-system-metrics-interval", t),
+});
