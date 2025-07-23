ad53a5cdd4b06032de10caa0c59dbf65fbdac7b2
John Wesley Quintero <johnwesleyquintero@gmail.com>
2025-07-23 14:38:13 +0800

    refactor: improve code readability and structure in main and preload scripts

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,22 +1,32 @@
-import { app as e, BrowserWindow as t } from "electron";
-import n from "path";
-function i() {
-  const o = new t({
+import { app, BrowserWindow } from "electron";
+import path from "path";
+function createWindow() {
+  const mainWindow = new BrowserWindow({
     width: 900,
     height: 600,
     webPreferences: {
-      preload: n.join(__dirname, "preload.cjs"),
-      nodeIntegration: !0,
-      contextIsolation: !1
+      preload: path.join(__dirname, "preload.cjs"),
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
-  process.env.VITE_DEV_SERVER_URL ? (o.loadURL(process.env.VITE_DEV_SERVER_URL), o.webContents.openDevTools()) : o.loadFile(n.join(__dirname, "dist", "index.html"));
+  if (process.env.VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "dist", "index.html"));
+  }
 }
-e.whenReady().then(() => {
-  i(), e.on("activate", () => {
-    t.getAllWindows().length === 0 && i();
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   });
 });
-e.on("window-all-closed", () => {
-  process.platform !== "darwin" && e.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
